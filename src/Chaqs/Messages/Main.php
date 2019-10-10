3b26538514428a16437b13190d05f9d2616a5ad7
Chaqs <41303233+Chaqs@users.noreply.github.com>
2019-10-10 15:54:22 +0100

    Add files via upload

---
+++ src/Chaqs/Messages/Main.php
@@ -0,0 +1,52 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Chaqs\Messages;
+
+use pocketmine\plugin\PluginBase;
+use pocketmine\command\CommandSender;
+use pocketmine\command\Command;
+use pocketmine\event\player\PlayerJoinEvent;
+use pocketmine\event\player\PlayerQuitEvent;
+use pocketmine\event\Listener;
+use pocketmine\Player;
+use pocketmine\utils\Config;
+
+
+class Main extends PluginBase implements Listener
+{
+
+    public function onEnable(): void
+    {
+        $this->getLogger()->info("Messages Has Been Enabled Author: Chaqs");
+        $this->getServer()->getPluginManager()->registerEvents($this, $this);
+        $this->saveDefaultConfig();
+    }
+
+    public function OnJoin(PlayerJoinEvent $event) {
+     $player = $event->getPlayer();
+     $name = $player->getName();
+        if($player->hasPlayedBefore()){
+         $player->sendMessage(str_replace('{name}', $name, $this->getConfig()->get("join-message-player")));
+         $event->setJoinMessage(str_replace('{name}', $name, $this->getConfig()->get("join-message-broadcast")));
+        }else{
+        $event->setJoinMessage(str_replace('{name}', $name, $this->getConfig()->get("first-join-broadcast")));
+        $player->sendMessage(str_replace('{name}', $name, $this->getConfig()->get("first-join-player")));
+
+          }
+       }
+
+    public function OnQuit(PlayerQuitEvent $event) {
+        $player = $event->getPlayer();
+        $name = $player->getName();
+        $event->setQuitMessage(str_replace('{name}', $name, $this->getConfig()->get("quit-message-broadcast")));
+    }
+
+
+
+
+	public function onDisable() : void{
+		$this->getLogger()->info("Messages Has Been Disabled");
+	}
+}
