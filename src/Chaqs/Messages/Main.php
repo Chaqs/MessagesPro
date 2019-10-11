e0bbaf9b2e3178bc05e2988cb01b8a6a125331ee
Chaqs <41303233+Chaqs@users.noreply.github.com>
2019-10-11 02:38:58 +0100

    Update Main.php

--- src/Chaqs/Messages/Main.php
+++ src/Chaqs/Messages/Main.php
@@ -12,6 +12,9 @@ use pocketmine\event\player\PlayerQuitEvent;
 use pocketmine\event\Listener;
 use pocketmine\Player;
 use pocketmine\utils\Config;
+use pocketmine\event\player\PlayerDeathEvent;
+use pocketmine\utils\TextFormat;
+use pocketmine\event\entity\EntityDamageEvent;
 
 
 class Main extends PluginBase implements Listener
@@ -19,34 +22,52 @@ class Main extends PluginBase implements Listener
 
     public function onEnable(): void
     {
-
         $this->getServer()->getPluginManager()->registerEvents($this, $this);
         $this->saveDefaultConfig();
     }
 
-    public function OnJoin(PlayerJoinEvent $event) {
-     $player = $event->getPlayer();
-     $name = $player->getName();
-        if($player->hasPlayedBefore()){
-         $player->sendMessage(str_replace('{name}', $name, $this->getConfig()->get("join-message-player")));
-         $event->setJoinMessage(str_replace('{name}', $name, $this->getConfig()->get("join-message-broadcast")));
-        }else{
-        $event->setJoinMessage(str_replace('{name}', $name, $this->getConfig()->get("first-join-broadcast")));
-        $player->sendMessage(str_replace('{name}', $name, $this->getConfig()->get("first-join-player")));
+    public function OnJoin(PlayerJoinEvent $event)
+    {
+        $player = $event->getPlayer();
+        $name = $player->getName();
+        if ($player->hasPlayedBefore()) {
+            $player->sendMessage(str_replace('{name}', $name, $this->getConfig()->get("join-message-player")));
+            $event->setJoinMessage(str_replace('{name}', $name, $this->getConfig()->get("join-message-broadcast")));
+        } else {
+            $event->setJoinMessage(str_replace('{name}', $name, $this->getConfig()->get("first-join-broadcast")));
+            $player->sendMessage(str_replace('{name}', $name, $this->getConfig()->get("first-join-player")));
 
-          }
-       }
+        }
+    }
 
-    public function OnQuit(PlayerQuitEvent $event) {
+    public function OnQuit(PlayerQuitEvent $event)
+    {
         $player = $event->getPlayer();
         $name = $player->getName();
         $event->setQuitMessage(str_replace('{name}', $name, $this->getConfig()->get("quit-message-broadcast")));
     }
 
+    public function onDeath(PlayerDeathEvent $event)
+    {
+        $player = $event->getPlayer();
+        $name = $player->getName();
+        $cause = $player->getLastDamageCause()->getCause();
+        if ($cause === EntityDamageEvent::CAUSE_SUFFOCATION) {
+            $event->setDeathMessage(str_replace('{name}', $name, $this->getConfig()->get("suffocation-death")));
+        } elseif ($cause === EntityDamageEvent::CAUSE_DROWNING) {
+            $event->setDeathMessage(str_replace('{name}', $name, $this->getConfig()->get("drown-death")));
+        } elseif ($cause === EntityDamageEvent::CAUSE_FALL) {
+            $event->setDeathMessage(str_replace('{name}', $name, $this->getConfig()->get("fall-damage-death")));
+        } elseif ($cause === EntityDamageEvent::CAUSE_FIRE) {
+            $event->setDeathMessage(str_replace('{name}', $name, $this->getConfig()->get("fire-damage-death")));
+        } elseif ($cause === EntityDamageEvent::CAUSE_LAVA) {
+            $event->setDeathMessage(str_replace('{name}', $name, $this->getConfig()->get("lava-death")));
+        } elseif ($cause === EntityDamageEvent::CAUSE_BLOCK_EXPLOSION) {
+            $event->setDeathMessage(str_replace('{name}', $name, $this->getConfig()->get("explosion-death")));
+        } else {
+            $event->setDeathMessage(str_replace('{name}', $name, $this->getConfig()->get("other-death")));
 
-
-
-	public function onDisable() : void{
-
-	}
+        }
+    }
 }
+
